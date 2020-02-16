0a1772e90787a9f9e5f0b976eadb418ef1ead5b0
adrianoc <adrianoc@oi.com.br>
2020-02-16 19:18:04 -0300

    melhorando os comentario no js padrao

--- webroot/js/lista.js
+++ webroot/js/lista.js
@@ -1,13 +1,14 @@
 window.onload = function()
 {
+	// capturando o evento click, para todos os elementos que possui a classe buttonPaginateAjax
 	Array.from( document.querySelectorAll(".buttonPaginateAjax" ), e =>
 	e.addEventListener('click', event =>
 	{
 		event.preventDefault()
-
 		getPaginate(e)
 	}))
 
+	// forçando o click da primeira página, em todos os formulários
 	let totalForms = document.querySelectorAll('[name="pAjaxP"]').length
 	for (i=1; i<=totalForms; i++)
 	{
@@ -15,6 +16,73 @@ window.onload = function()
 	}
 }
 
+/**
+ * Execua a pesquisa ajax.
+ *
+ * @param 	e 	Elemento button (<<, <, >, >>)
+ */
+async function getPaginate(e)
+{
+	let elemento 	= e.form
+	let ajaxUrl 	= e.form.action
+	let currentPage = elemento.querySelector('[name="pagina"]').getAttribute('value')
+	let lastPage 	= parseInt( elemento.querySelector('[name="ultima"]').getAttribute('value') )
+	let acoes 		= {}
+	let token 		= {}
+
+	// configurando o token caso exista
+	if ( !!elemento.querySelector('[name="_Token[fields]"]') )
+	{
+		token['fields'] = elemento.querySelector('[name="_Token[fields]"]').getAttribute('value')
+	}
+	if ( !!elemento.querySelector('[name="_Token[unlocked]"]') )
+	{
+		token['unlocked'] = elemento.querySelector('[name="_Token[unlocked]"]').getAttribute('value')
+	}
+	if ( !!elemento.querySelector('[name="_Token[debug]"]') )
+	{
+		token['debug'] = elemento.querySelector('[name="_Token[debug]"]').getAttribute('value')
+	}
+
+	// configurando a página clicada pelo usuário
+	if ( e.name == 'pAjaxP') currentPage = 1
+	if ( e.name == 'pAjaxA') currentPage--
+	if ( e.name == 'pAjaxR') currentPage++
+	if ( e.name == 'pAjaxU') currentPage = lastPage
+	if ( currentPage > lastPage && lastPage>0 ) currentPage = lastPage
+	if ( currentPage < 1) currentPage = 1
+
+	let spanPagina 	= elemento.querySelector('[id="spanPagina"]')
+	let spanTotal 	= elemento.querySelector('[id="spanTotal"]')
+	let spanFaixa  	= elemento.querySelector('[id="spanFaixa"]')
+	let spanUltima  = elemento.querySelector('[id="spanUltima"]')
+
+	elemento.querySelector('[name="pagina"]').setAttribute('value', currentPage)
+
+	let ajaxForm 	= JSON.stringify({'pagina': currentPage, 'ultima': lastPage, '_Token': token })
+	let ajaxOptions = { body: ajaxForm, method: 'post', headers: {'Content-Type': 'application/json' } }
+
+	// executando o ajax e populando seu retorno na table ou seu erro.
+	await fetch(ajaxUrl, ajaxOptions)
+	.then( res => res.json() )
+	.then( res => 
+	{
+		if ( res.status )
+		{
+			setPaginateTable(elemento, res)
+		} else
+		{
+			elemento.querySelector('[name="thead"]').innerHTML = "<tr><th>erro</th></tr>"
+			elemento.querySelector('[name="tbody"]').innerHTML = "<tr><td class='PaginateAjaxError'>"+res.mensagem+"</td></tr>"
+		}
+	}).catch( error => console.error(`Error: ${error}`) )
+}
+
+/**
+ * Executa a ação, escolhida pelo usuário.
+ *
+ * param 	e 	Elemento select
+ */
 function setPaginateAction(e)
 {
 	let opcao = e.selectedIndex
@@ -32,14 +100,14 @@ function setPaginateAction(e)
 	}
 }
 
-async function getPaginate(e)
+/**
+ * Escreve na tabela o resultado da pesquisa ajax
+ *
+ * param 	elemento 		Elemento button (<<, <, >, >>)
+ * param 	res 			Resposta da pesquisas ajax.
+ */
+function setPaginateTable(elemento, res)
 {
-	let elemento 	= e.form
-
-	let ajaxUrl 	= e.form.action
-	let currentPage = elemento.querySelector('[name="pagina"]').getAttribute('value')
-	let lastPage 	= parseInt( elemento.querySelector('[name="ultima"]').getAttribute('value') )
-	let acoes 		= {}
 	let linkAcoes 	= '';
 	if ( typeof elemento.querySelector('[name="acoes"]') != 'undefined' )
 	{
@@ -53,95 +121,38 @@ async function getPaginate(e)
 		linkAcoes  	+= "</select>"
 	}
 
-	let token = {}
-	if ( !!elemento.querySelector('[name="_Token[fields]"]') )
+	spanPagina.textContent 	= res.paginacao.pagina
+	spanTotal.textContent 	= res.paginacao.total.toLocaleString()
+	spanFaixa.textContent 	= res.paginacao.faixa
+	spanUltima.textContent 	= res.paginacao.ultima.toLocaleString()
+	elemento.querySelector('[name="ultima"]').setAttribute('value', res.paginacao.ultima)
+
+	let linesTbody 	= ""
+	let linesThead 	= ""
+	let ll 			= 0
+	Object.keys(res.lista).forEach( (l, linha) =>
 	{
-		token['fields'] = elemento.querySelector('[name="_Token[fields]"]').getAttribute('value')
-	}
-	if ( !!elemento.querySelector('[name="_Token[unlocked]"]') )
-	{
-		token['unlocked'] = elemento.querySelector('[name="_Token[unlocked]"]').getAttribute('value')
-	}
-	if ( !!elemento.querySelector('[name="_Token[debug]"]') )
-	{
-		token['debug'] = elemento.querySelector('[name="_Token[debug]"]').getAttribute('value')
-	}
-
-	switch (e.name)
-	{
-		case 'pAjaxP':
-			currentPage = 1
-		break
-
-		case 'pAjaxA':
-			currentPage--
-		break
-
-		case 'pAjaxR':
-			currentPage++
-		break
-
-		case 'pAjaxU':
-			currentPage = lastPage
-		break
-	}
-	if ( currentPage > lastPage && lastPage>0) currentPage = lastPage
-	if ( currentPage < 1) currentPage = 1
-
-	let spanPagina 	= elemento.querySelector('[id="spanPagina"]')
-	let spanTotal 	= elemento.querySelector('[id="spanTotal"]')
-	let spanFaixa  	= elemento.querySelector('[id="spanFaixa"]')
-	let spanUltima  = elemento.querySelector('[id="spanUltima"]')
-
-	elemento.querySelector('[name="pagina"]').setAttribute('value', currentPage)
-
-	let ajaxForm 	= JSON.stringify({'pagina': currentPage, 'ultima': lastPage, '_Token': token })
-	let ajaxOptions = { body: ajaxForm, method: 'post', headers: {'Content-Type': 'application/json' } }
-
-	await fetch(ajaxUrl, ajaxOptions)
-		.then( res => res.json() )
-		.then( res => 
+		let cloneLinkAcoes = linkAcoes
+		linesTbody += "<tr>"
+		Object.keys(res.lista[linha]).forEach( (cmp) =>
 		{
-			if ( res.status )
-			{
-				spanPagina.textContent 	= res.paginacao.pagina
-				spanTotal.textContent 	= res.paginacao.total
-				spanFaixa.textContent 	= res.paginacao.faixa
-				spanUltima.textContent 	= res.paginacao.ultima
-				elemento.querySelector('[name="ultima"]').setAttribute('value', res.paginacao.ultima)
+			let vlr = res.lista[linha][cmp]
+			if ( !ll ) { linesThead += "<th>"+cmp+"</th>" }
+			linesTbody += "<td>"+vlr+"</td>"
+			cloneLinkAcoes = cloneLinkAcoes.replace( new RegExp(`{${cmp}}`, 'g') , vlr)
+		})
 
-				let linesTbody 	= ""
-				let linesThead 	= ""
-				let ll 			= 0
-				Object.keys(res.lista).forEach( (l, linha) =>
-				{
-					let cloneLinkAcoes = linkAcoes
-					linesTbody += "<tr>"
-					Object.keys(res.lista[linha]).forEach( (cmp) =>
-					{
-						let vlr = res.lista[linha][cmp]
-						if ( !ll ) { linesThead += "<th>"+cmp+"</th>" }
-						linesTbody += "<td>"+vlr+"</td>"
-						cloneLinkAcoes = cloneLinkAcoes.replace( new RegExp(`{${cmp}}`, 'g') , vlr)
-					})
+		if ( linkAcoes.length )
+		{
+			linesTbody += "<td class='tdPaginateAjaxAcao'>"+cloneLinkAcoes+"</td>"
+		}
 
-					if ( linkAcoes.length )
-					{
-						linesTbody += "<td class='tdPaginateAjaxAcao'>"+cloneLinkAcoes+"</td>"
-					}
+		linesTbody += "</tr>"
+		ll++
+	})
 
-					linesTbody += "</tr>"
-					ll++
-				})
+	if ( linkAcoes.length ) { linesThead += "<th name='thAcoes' class='thPaginateAjaxAcoes'>Ações</th>" }
 
-				if ( linkAcoes.length ) { linesThead += "<th name='thAcoes' class='thPaginateAjaxAcoes'>Ações</th>" }
-
-				elemento.querySelector('[name="thead"]').innerHTML = "<tr>"+linesThead+"</tr>"
-				elemento.querySelector('[name="tbody"]').innerHTML = linesTbody
-			} else
-			{
-				elemento.querySelector('[name="thead"]').innerHTML = "<tr><th>erro</th></tr>"
-				elemento.querySelector('[name="tbody"]').innerHTML = "<tr><td class='PaginateAjaxError'>"+res.mensagem+"</td></tr>"
-			}
-		}).catch( error => console.error(`Error: ${error}`) )
+	elemento.querySelector('[name="thead"]').innerHTML = "<tr>"+linesThead+"</tr>"
+	elemento.querySelector('[name="tbody"]').innerHTML = linesTbody
 }
